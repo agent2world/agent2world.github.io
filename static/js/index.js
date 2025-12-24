15e02780e7857df48d58da8bbb0e4ce2aa025b9b
subway-jack <549451399@qq.com>
2025-12-24 21:25:08 +0800

    update the index.html

--- static/js/index.js
+++ static/js/index.js
@@ -26,6 +26,8 @@ $(document).ready(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
+      var expanded = $(".navbar-burger").hasClass("is-active");
+      $(".navbar-burger").attr("aria-expanded", expanded ? "true" : "false");
 
     });
 
